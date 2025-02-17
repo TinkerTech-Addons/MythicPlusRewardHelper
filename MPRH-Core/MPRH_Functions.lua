d5c601ffc50b706db91ea8f40150d255eca525b3
Dillon Louden <djlouden@mix.wvu.edu>
2025-02-17 01:03:46 -0500

    updates to ilvls crests and outputs for season 2

--- MPRH-Core/MPRH_Functions.lua
+++ MPRH-Core/MPRH_Functions.lua
@@ -14,12 +14,13 @@ function ns.buildTooltip(keystone_level, tooltip)
         keystone_data[2].b * 255)
 
     -- Great Vault
-    local GV_Color = string.format("|cff%02x%02x%02x", keystone_data[5].r * 255, keystone_data[5].g * 255,
-        keystone_data[5].b * 255)
+    local GV_Color = string.format("|cff%02x%02x%02x", keystone_data[6].r * 255, keystone_data[6].g * 255,
+        keystone_data[6].b * 255)
 
     tooltip:AddLine("|cDBB670FFMythic Plus Reward Helper|r")
-    tooltip:AddLine("End of Dungeon: " .. EoD_Color .. keystone_data[1] .. " " .. keystone_data[3] .. "|r")
-    tooltip:AddLine("Great Vault: " .. GV_Color .. keystone_data[4] .. "|r")
+    tooltip:AddLine("End of Dungeon: " ..
+        EoD_Color .. keystone_data[1] .. " " .. keystone_data[3] .. "x" .. keystone_data[4] .. "|r")
+    tooltip:AddLine("Great Vault: " .. GV_Color .. keystone_data[5] .. "|r")
 end
 
 function ns.buildSlashInfo(keystone_level)
@@ -39,11 +40,12 @@ function ns.buildSlashInfo(keystone_level)
         keystone_data[2].b * 255)
 
     -- Great Vault
-    local GV_Color = string.format("|cff%02x%02x%02x", keystone_data[5].r * 255, keystone_data[5].g * 255,
-        keystone_data[5].b * 255)
+    local GV_Color = string.format("|cff%02x%02x%02x", keystone_data[6].r * 255, keystone_data[6].g * 255,
+        keystone_data[6].b * 255)
 
     print("|cDBB670FFMythic Plus Reward Helper|r")
     print("|cFF14cd33Keystone Level:|r " .. keystone_level)
-    print("|cFF14cd33End of Dungeon:|r " .. EoD_Color .. keystone_data[1] .. " " .. keystone_data[3] .. "|r")
-    print("|cFF14cd33Great Vault:|r " .. GV_Color .. keystone_data[4] .. "|r")
+    print("|cFF14cd33End of Dungeon:|r " ..
+        EoD_Color .. keystone_data[1] .. " " .. keystone_data[3] .. "x" .. keystone_data[4] .. "|r")
+    print("|cFF14cd33Great Vault:|r " .. GV_Color .. keystone_data[5] .. "|r")
 end
